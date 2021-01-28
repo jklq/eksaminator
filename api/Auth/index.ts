1c9df5864fb656262a5d3828c5c380b5d0cbeb64
jklq <mail@seang.xyz>
2021-01-28 09:31:30 +0100

    started an Auth function

---
+++ api/Auth/index.ts
@@ -0,0 +1,13 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+
+    
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: req
+    };
+
+};
+
+export default httpTrigger;
