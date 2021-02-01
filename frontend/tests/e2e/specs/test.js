cb3773d0f8cd7fd205588a8095c0c033799f10d2
jklq <mail@seang.xyz>
2021-02-01 07:46:55 +0100

    added tests for registration and frontpage

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -1,8 +1,15 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
+describe('Not logged in', () => {
   it('Visits the app root url', () => {
     cy.visit('/');
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App');
+    cy.contains('h1', 'Din Personlige Eksamen-hjelper')
+
+    it('Contains a working login button', () => {
+      cy.contains("Logg inn").should('have.attr', 'href', '/login')
+    })
+    it('Contains a working register button', () => {
+      cy.contains("Lag en gratis bruker").should('have.attr', 'href', '/register') 
+    })
   });
 });
---
+++ frontend/tests/e2e/specs/userActions.js
@@ -0,0 +1,51 @@
+
+describe('Registration', () => {
+  cy.visit('/register')
+
+  it('Contains all the correct fields and a title', () => {
+    cy.contains('h1', 'Lag en bruker')
+    cy.contains('label', 'Navn').should('not.have.attr', 'required')
+    cy.contains('label', 'Brukernavn').should('have.attr', 'required')
+    cy.contains('label', 'E-post').should('have.attr', 'required')
+    cy.contains('label', 'Passord').should('have.attr', 'required')
+    cy.contains('label', 'Bekreft Passord').should('have.attr', 'required')
+  })
+
+  context('Registration with Invalid Inputs', () => {
+    beforeEach(() => {
+      cy.get('#name-input').type('John Doe')
+      cy.get('#username-input').type('jklq0')
+      cy.get('#email-input').type('email@email.com')
+      cy.get('#password-input').type('ValidValidPassword123321')
+      cy.get('#password-confirmation-input').type('ValidValidPassword123321')
+    })
+    afterEach(() => {
+      cy.get('#submit-button').click()
+      cy.get('.error').should('be.visible')
+    })
+
+    it('Does not work to register with invalid username', () => {
+      cy.get('#username-input').type('jk')
+    })
+
+    it('Does not work to register with invalid password', () => {
+      cy.get('#password-input').type('123')
+      cy.get('#password-confirmation-input').type('123')
+    })
+
+    it('Does not work to register with non-matching passwords', () => {
+      cy.get('#password-input').type('ThisPasswordIsValid172')
+      cy.get('#password-confirmation-input').type('ThisPasswordIsAlsoValid1231')
+    })
+  })
+
+  it('Works to register with valid details', () => {
+    cy.get('#name-input').type('John Doe')
+    cy.get('#username-input').type('jklq0')
+    cy.get('#email-input').type('email@email.com')
+    cy.get('#password-input').type('ValidValidPassword123321')
+    cy.get('#password-confirmation-input').type('ValidValidPassword123321')
+    cy.get('#submit-button').click()
+    cy.url().should('include', 'app')
+  })
+})
